1b561ab839bf481783e5399448439d4d7a211185
keepcalm444 <keepcalm444@gmail.com>
2014-12-03 08:54:35 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#cloaked-octo-tyrion
+it's like telnet but it proxies through a hardcoded SOCKS proxy of
+localhost:1080
+
+##coming soon
+* non-hardcodedness
+
+
---
+++ socks.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+	"os"
+	"bytes"
+	"strings"
+	"encoding/binary"
+	"strconv"
+)
+
+func genRequest(dns string, port uint16) []byte {
+	reader := bytes.NewBufferString(dns)
+	bts := reader.Bytes()
+	res := []byte{0x05, 0x01, 0x00, 0x03, byte(len(bts))}
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, port)
+	res = append(res, bts...)
+	fmt.Sprintln("%x", res)
+	return append(res, b...) // TODO ports other than 80
+}
+
+func sendStuff(conn net.Conn) {
+	stdin := bufio.NewReader(os.Stdin)
+	for true {
+		text, _ := stdin.ReadString('\n')
+		fmt.Fprintf(conn, strings.TrimSpace(text) + "\r\n")
+	}
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:1080")
+	if len(os.Args) < 3 {
+		println("Usage: " + os.Args[0] + " <domain name> <port>")
+		os.Exit(1)
+	}
+	if err != nil {
+		fmt.Println("Damn")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	conn.Write([]byte{0x05, 0x01, 0x00}) // SOCKS 5, no auth
+	response := make([]byte, 2)
+	conn.Read(response)
+	if response[0] != 0x05 {
+		fmt.Println("Not SOCKS 5")
+		fmt.Println("It's actually SOCKS %d", response[0])
+		os.Exit(2)
+	}
+
+	if response[1] == 0xFF {
+		fmt.Println("No acceptable auth methods")
+		os.Exit(2)
+	}
+
+	// try and open a TCP connection to google or something
+	port, err := strconv.ParseInt(os.Args[2], 10, 16)
+	if err != nil {
+		println("Invalid port " + os.Args[2])
+		os.Exit(3)
+	}
+	toSend := genRequest(os.Args[1], uint16(port))
+	conn.Write(toSend)
+	fmt.Println("Trying " + os.Args[1] + ":" + os.Args[2] + "...")
+	response = make([]byte, 4)
+	conn.Read(response)
+	msg := "Unknown error"
+	switch response[1] {
+	case 0x00:
+		msg = "Success"
+	case 0x01:
+		msg = "General SOCKS Server Failure"
+	case 0x02:
+		msg = "Connection not allowed by ruleset"
+	case 0x03:
+		msg = "Network unreachable"
+	case 0x04:
+		msg = "Host unreachable"
+	case 0x05:
+		msg = "Connection refused"
+	case 0x06:
+		msg = "TTL expired"
+	case 0x07:
+		msg = "Command not supported"
+	case 0x08:
+		msg = "Address type not supported"
+	}
+	if response[1] != 0 {
+		fmt.Println("Error:",msg)
+		os.Exit(1)
+	}
+	println("Connected!")
+	// now do some reading and writing
+	go sendStuff(conn)
+	reader := bufio.NewReader(conn)
+	for true {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			println("Lost connection to the SOCKS server")
+			println(err)
+			os.Exit(3)
+		}
+		line = strings.TrimSpace(line)
+		println(line)
+	}
+
+}
